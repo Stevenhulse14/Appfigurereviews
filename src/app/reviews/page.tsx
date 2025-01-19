06c32c563d32c1e72269a86fe7273f656a5acdc3
steven <shotta3xd@gmail.com>
2025-01-19 16:14:18 -0500

    created Cards for Reviews and added logo and styling to the project, also created a sidebar

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ReviewList from "@/components/ReviewList";
+import ReviewSidebar from "@/components/ReviewSidebar";
 import KeywordFilter from "@/components/KeywordFilter";
 import StarFilter from "@/components/StarFilter";
 
@@ -10,11 +11,16 @@ export default function Reviews() {
       <h5 className="text-xl font-bold text-orange-light mb-2 text-center [text-shadow:_-1px_-1px_0_#000,_1px_-1px_0_#000,_-1px_1px_0_#000,_1px_1px_0_#000]">
         Twitter App Reviews
       </h5>
-      <div className="flex justify-end gap-4 mb-4">
+      <div className="flex flex-col sm:flex-row justify-end gap-4 mb-4">
         <KeywordFilter />
         <StarFilter />
       </div>
-      <ReviewList />
+      <div className="flex gap-4">
+        <ReviewSidebar />
+        <div className="flex-1 min-w-0">
+          <ReviewList />
+        </div>
+      </div>
     </main>
   );
 }
